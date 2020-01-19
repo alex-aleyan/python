432be2f4d6b6445d9e8925cc966c394d0878169d
alex-aleyan <root@cobalt-vm.acct04.us.lmco.com>
2020-01-19 18:35:10 -0500

    done with ch08; added module_funcs.py file

--- books/python_crush_course/ch_08/08_00_functions.py
+++ books/python_crush_course/ch_08/08_00_functions.py
@@ -497,81 +497,8 @@ print("##############################################################\n" + \
       "#                         chapter 7 USER IO                  #\n" + \
       "##############################################################\n"   )
 
+print("See content of functions found in module_funcs.py")
 
-
-# Creating a user and storing his info temporarily 
-# in new_users dictinary. Once confirmed, the user's
-# info is moved to actual_users dictionary. Where the
-# actual_users={<name_key> : <info_dictionary>}
-#     name_key - first character of the first name + last name
-#     info_dictionary - first_name : <user's first name>
-#                       last_name  : <user's last name>
-#                       age        : <user's age>
-#                       password   : <user's password>
-
-user_prompt="Let's personalize the messages.\n"
-print(user_prompt)
-actual_users={}
-
-for i in [1, 2]:
-
-    break
-
-    new_user={}
-    
-    #GET FIRST NAME:
-    user_prompt="First, let us know you first name: "
-    while True: #make sure the answer has no numbers in the name
-        user_answer=raw_input(user_prompt)
-        if user_answer.isalpha(): break    
-    print( "You entered: \"" + str(user_answer).title() + "\"" )
-    new_user['first_name']=user_answer.lower()
-    
-    #GET LAST NAME:
-    user_prompt="Now, let us know you last name: "
-    while True: #make sure the answer has no numbers in the name
-        user_answer=raw_input(user_prompt)
-        if user_answer.isalpha(): break    
-    print( "You entered: \"" + str(user_answer).title() + "\"" )
-    new_user['last_name']=user_answer.lower()
-    
-    #GET AGE:
-    #Loops if age has non-integer value until age is provided as an integer
-    while True:
-        try:
-            user_prompt="Please provide your age: "
-            user_answer=int(raw_input(user_prompt))
-        except ValueError:
-            print("Sorry, it gotta be an integer")
-        else: break
-    print( "You entered: \"" + str(user_answer) + "\"" )
-    new_user['age']=user_answer
-    
-    #GET PASSWORD:
-    while True: # Loop until password with no white space is provide:
-        user_prompt="Provide a password:"
-        user_answer=raw_input(user_prompt)
-        if (' ' in user_answer) != True: break
-    print( "You entered: \"" + str(user_answer) + "\"" )
-    new_user['password']=user_answer
-    
-    #ACCEPT THE PROVIDED DATA:
-    for key, data in new_user.items(): # show entered data
-        print( str(key.replace("_"," ")) + ": " + str(data) )
-    user_prompt="Accept the provided data(yes/no)?"
-    while True: #make sure the answer has no numbers in the name
-        user_answer=raw_input(user_prompt)
-        if user_answer.lower() == 'yes' or user_answer.lower == 'y': #move to actual_users
-            print("Saving the new user")
-            actual_users[ str( new_user['first_name'][0] + new_user['last_name'] ) ]=new_user
-            break    
-        if user_answer.lower() == 'no' or user_answer.lower() == 'n': #dismiss info
-            print("Dismissing the provied info")
-            break    
-    
-    for key in actual_users.keys():
-        print(key + " = " + str(actual_users[key]))
-    #print(actual_users[aaleyan][first_name])
 
 
 
@@ -580,88 +507,19 @@ print("##############################################################\n" + \
       "#                         chapter 8 FUNCTIONs                #\n" + \
       "##############################################################\n"   )
 
-new_user={}
+import module_funcs as funcs
+
 actual_users={}
-DEBUG=True #False
+temp_user=funcs.get_newuser()
 
-def get_alpha(user_prompt='gets abc..xyzABC..XYZ'):
-    while True: #make sure the answer has no numbers in the name
-        user_answer=raw_input(user_prompt)
-        if user_answer.isalpha(): break    
-    return user_answer
-
-def get_numeric(user_prompt='gets 0-9'):
-    #Loops if age has non-integer value until age is provided as an integer
-    while True:
-        try:
-            user_answer=int(raw_input(user_prompt))
-        except ValueError:
-            print("Sorry, it has gotten to be an integer")
-        else: break
-    return user_answer
-
-def get_alphanumeric(user_prompt='gets 0..9abc...xyzABC...XYZ'):
-    while True: # Loop until password with no white space is provide:
-        user_answer=raw_input(user_prompt)
-        if (' ' in user_answer) != True: break
-    return user_answer
-
-def get_username(first_name, last_name):
-    return str( first_name[0] + last_name )
-
-
-def get_newuser():
-    user_prompt="Provide your first name: "
-    new_user['first_name']=get_alpha(user_prompt).lower()
-    print(new_user['first_name'])
-    
-    user_prompt="Provide your last name: "
-    new_user['last_name']=get_alpha(user_prompt).lower()
-    print(new_user['last_name'])
-    
-    user_prompt="Please provide your age: "
-    new_user['age']=get_numeric(user_prompt)
-    print(new_user['age'])
-    
-    import getpass
-    user_prompt="Please provide a password: "
-    #new_user['password']=get_alphanumeric(user_prompt)
-    new_user['password']=getpass.getpass(user_prompt)
-    print(new_user['password'])
-    
-    #ACCEPT THE PROVIDED DATA:
-    print("\nShow provided data:")
-    for key, data in new_user.items(): # show entered data
-        if key != 'password': print( str(key.replace("_"," ")) + ": " + str(data) )
-        #print( str(key.replace("_"," ")) + ": " + str(data) )
-    
-    user_prompt="Accept the provided data(yes/no)?"
-    while True: 
-        user_answer=raw_input(user_prompt)
-        if user_answer.lower() == 'yes' or user_answer.lower == 'y': #move to actual_users
-            print("Saving the new user")
-            #actual_users[ get_username( new_user['first_name'],new_user['last_name'] ) ] = new_user
-            return new_user
-            break    
-        if user_answer.lower() == 'no' or user_answer.lower() == 'n': #dismiss info
-            print("Dismissing the provied info")
-            break    
-
-
-actual_users[ get_username( new_user['first_name'],new_user['last_name'] ) ] = get_newuser()
+actual_users[ funcs.get_username( temp_user['first_name'], temp_user['last_name'] ) ] = temp_user
 print("\nPrinting the content of actual_users dictionary:")
 for key in actual_users.keys():
     print(key + " = " + str(actual_users[key]))
 
 
 print("Using func(*args) to accept variable number of arguments as TUPLE!")
-def make_pizza(size, 
-               *toppings):
-    print("Pizza size: " + str(size) + "\"")
-    print("Print the toppings:")
-    for topping in toppings:
-        print( "- " + str(topping) )
 
-make_pizza(16,"mushrooms", "chicken")
+funcs.make_pizza(16,"mushrooms", "chicken")
 
 
