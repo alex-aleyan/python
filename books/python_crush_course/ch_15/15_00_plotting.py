f1258e5cfcb7b35c6191822f399b3aaf5d78705a
alex-aleyan <root@cobalt-vm.acct04.us.lmco.com>
2020-01-21 04:50:53 -0500

    still on the WALK

--- books/python_crush_course/ch_15/15_00_plotting.py
+++ books/python_crush_course/ch_15/15_00_plotting.py
@@ -82,6 +82,13 @@ rw.fill_walk()
 point_numbers=list(range(rw.num_points))
 pyplot.scatter(rw.x_values, rw.y_values, 
                c=point_numbers, cmap=pyplot.cm.Blues, edgecolor='none',  s=1)
+pyplot.scatter(0, 0, c='green', edgecolors='none', s=100) 
+pyplot.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100) 
 #pyplot.plot(rw.x_values, rw.y_values,
 #               c=point_numbers, cmap=pyplot.cm.Blues, edgecolor='none',  s=1)
+
+#Remove the axes:
+#pyplot.axes().get_xaxis().set_visible(False)
+#pyplot.axes().get_yaxis().set_visible(False)
+
 pyplot.show()
