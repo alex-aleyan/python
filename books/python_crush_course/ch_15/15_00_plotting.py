b16e49e0b7c198283f3849ebbb5338c1990cadef
alex-aleyan <root@cobalt-vm.acct04.us.lmco.com>
2020-01-21 01:58:32 -0500

    still working on ch15

--- books/python_crush_course/ch_15/15_00_plotting.py
+++ books/python_crush_course/ch_15/15_00_plotting.py
@@ -1,13 +1,38 @@
 import matplotlib.pyplot as pyplot
 
-squares=[1,4,9,16,25]
+x_values=[1,2,3,4,5]
+y_values=[x**2 for x in x_values]
 
-pyplot.plot(squares, linewidth=1)
-
-pyplot.title("Square Numbers",   fontsize=24)
+pyplot.title("(1) Plot: Square Numbers",   fontsize=24)
 pyplot.xlabel("Value",           fontsize=14)
 pyplot.ylabel("Square of Value", fontsize=14)
-
 pyplot.tick_params(axis='both', labelsize=8)
+pyplot.plot(x_values, y_values, linewidth=1)
+
+pyplot.show()
+
+pyplot.title("(2) Scatter: Square Numbers",   fontsize=24)
+pyplot.xlabel("Value",           fontsize=14)
+pyplot.ylabel("Square of Value", fontsize=14)
+pyplot.tick_params(axis='both', labelsize=8)
+pyplot.scatter(x_values, y_values) #,s=200
+
+pyplot.show()
+
+pyplot.title("(3) Plot+Scatter: Squared Numbers",   fontsize=24)
+pyplot.xlabel("Value",           fontsize=14)
+pyplot.ylabel("Square of Value", fontsize=14)
+pyplot.tick_params(axis='both', which='major', labelsize=8)
+pyplot.plot(x_values, y_values, linewidth=1)
+pyplot.scatter(x_values, y_values, s=50)
+pyplot.axis([0, 6, 0, 30])
+
+pyplot.show()
+
+pyplot.title("(4) Plot+Scatter: Large Range!",   fontsize=24)
+x_values=list(range(1,1000))
+y_values=[x**2 for x in x_values]
+pyplot.scatter(x_values, y_values, s=1)
+pyplot.axis([0, 1100, 0, (1000**2)+100])
 
 pyplot.show()
