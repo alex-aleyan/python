e7776179b63d2408b060b37b0ea1fe5f910f2ee0
alex-aleyan <root@cobalt-vm.acct04.us.lmco.com>
2020-01-20 19:24:38 -0500

    added middle name

--- books/python_crush_course/ch_11/module_dut.py
+++ books/python_crush_course/ch_11/module_dut.py
@@ -1,6 +1,9 @@
-def get_formatted_name(first, last):
+def get_formatted_name(first, last, middle=""):
     "generate a neatly formatted full name"
-    full_name=first + " " + last
+    if middle:
+        full_name=first + " " + middle + " " + last
+    else:
+        full_name=first + " " + last
     return full_name.title()
 
 
--- books/python_crush_course/ch_11/module_tb.py
+++ books/python_crush_course/ch_11/module_tb.py
@@ -9,6 +9,11 @@ class NamesTestCase(unittest.TestCase):
         formatted_name = dut.get_formatted_name('janis','joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
 
+    def test_first_last_middle_name(self):
+        "\"Do names like Wolfgang Amadeus Mozart work?\""
+        formatted_name = dut.get_formatted_name('Wolfgang','Mozart','Amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
 #unittest.main()
 if __name__ == "__main__":
     unittest.main()
