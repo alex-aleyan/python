35a2db69f93c86bdce2859c4f6e82467b7b535f5
alex-aleyan <root@cobalt-vm.acct04.us.lmco.com>
2020-01-20 23:37:52 -0500

    finished testing chapter

--- books/python_crush_course/ch_11/module_tb.py
+++ books/python_crush_course/ch_11/module_tb.py
@@ -34,16 +34,29 @@ class NamesTestCase(unittest.TestCase):
         formatted_name = dut.get_formatted_name('Wolfgang','Mozart','Amadeus')
         self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
 
+
 class TestAnonSurvey(unittest.TestCase):
     "\"Tests for the class AnonSurvey\""
 
+    def setUp(self):
+        question="What language did you first learn to speak?"
+        self.my_survey=dut.AnonSurvey(question)
+        self.responses=['English', 'Spanish', 'Mandarin']
+
     def test_store_single_response(self):
         "\"Test that a single response is stored properly\""
-        question="What language did you first learn to speak?"
-        my_survey=dut.AnonSurvey(question)
-        my_survey.store_response('English')
+        self.my_survey.store_response('English')
+        self.assertIn('English', self.my_survey.responses)
 
-        self.assertIn('English', my_survey.responses)
+    def test_store_three_responses(self):
+        "\"Test that three individual responses are stored properly.\""
+
+        for response in self.responses:
+            self.my_survey.store_response(response)
+
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
+        
 
 #unittest.main()
 if __name__ == "__main__":
