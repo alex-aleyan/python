99e312279b40339f5fe68ab56e5c1d7e6627ef45
alex-aleyan <root@cobalt-vm.acct04.us.lmco.com>
2020-01-20 20:37:54 -0500

    testing a class

--- books/python_crush_course/ch_11/module_tb.py
+++ books/python_crush_course/ch_11/module_tb.py
@@ -1,6 +1,26 @@
 import unittest
 import module_dut as dut
 
+# unittest.TestCase ASSERTS:
+'''
+assertEqual(a,b)
+assertNotEqual(a,b)
+
+assertTrue(x)
+assertFalse(x)
+
+assertIs(a,b)
+assertIsNot(a,b)
+assertIsNone(x)
+assertIsNotNone(x)
+
+assertIn(a,b)
+assertNotIn(a,b)
+
+assertIsInstance(a,b)
+assertNotIsInstance(a,b)
+'''
+
 class NamesTestCase(unittest.TestCase):
     "\"Test for name_function.py\""
     
@@ -13,6 +33,17 @@ class NamesTestCase(unittest.TestCase):
         "\"Do names like Wolfgang Amadeus Mozart work?\""
         formatted_name = dut.get_formatted_name('Wolfgang','Mozart','Amadeus')
         self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
+class TestAnonSurvey(unittest.TestCase):
+    "\"Tests for the class AnonSurvey\""
+
+    def test_store_single_response(self):
+        "\"Test that a single response is stored properly\""
+        question="What language did you first learn to speak?"
+        my_survey=dut.AnonSurvey(question)
+        my_survey.store_response('English')
+
+        self.assertIn('English', my_survey.responses)
 
 #unittest.main()
 if __name__ == "__main__":
